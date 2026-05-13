bea883d1b746a063d2e5db5675be95e605e4af71
Yuuki Takano <ytakanoster@gmail.com>
2026-05-13 15:42:56 +0000

    add env checker soundness surface

--- rocq/theories/TypeSystem/EnvTypingRules.v
+++ rocq/theories/TypeSystem/EnvTypingRules.v
@@ -62,6 +62,19 @@ Inductive borrow_ok_env (env : global_env)
       borrow_check_env env PBS Γ e = infer_ok PBS' ->
       borrow_ok_env env PBS Γ e PBS'.
 
+Definition typed_fn_env (env : global_env) (f : fn_def) : Prop :=
+  exists T_body Γ_out,
+    typed_env env (fn_outlives f) (fn_lifetimes f)
+      (sctx_of_ctx (params_ctx (fn_params f)))
+      (fn_body f) T_body (sctx_of_ctx Γ_out) /\
+    ty_compatible_b (fn_outlives f) T_body (fn_ret f) = true /\
+    params_ok_b (fn_params f) Γ_out = true.
+
+Definition checked_fn_env (env : global_env) (f : fn_def) : Prop :=
+  typed_fn_env env f /\
+  exists PBS',
+    borrow_ok_env env [] (params_ctx (fn_params f)) (fn_body f) PBS'.
+
 (* ------------------------------------------------------------------ *)
 (* Local soundness of executable env checker                             *)
 (* ------------------------------------------------------------------ *)
@@ -86,6 +99,45 @@ Lemma infer_core_env_state_fuel_sound :
     typed_env env Ω n Σ e T Σ'.
 Proof.
   intros. econstructor. exact H.
+Qed.
+
+Theorem infer_core_env_sound : forall env Ω n Γ e T Γ',
+  infer_core_env env Ω n Γ e = infer_ok (T, Γ') ->
+  typed_env env Ω n (sctx_of_ctx Γ) e T (sctx_of_ctx Γ').
+Proof.
+  unfold infer_core_env, sctx_of_ctx, ctx_of_sctx.
+  intros env Ω n Γ e T Γ' H.
+  destruct (infer_core_env_state_fuel 10000 env Ω n Γ e)
+    as [[T0 Γ0] | err] eqn:Hcore; inversion H; subst.
+  eapply infer_core_env_state_fuel_sound. exact Hcore.
+Qed.
+
+Theorem infer_env_sound : forall env f T Γ',
+  infer_env env f = infer_ok (T, Γ') ->
+  typed_fn_env env f.
+Proof.
+  unfold infer_env, typed_fn_env.
+  intros env f T Γ' H.
+  destruct (negb (wf_outlives_b (mk_region_ctx (fn_lifetimes f)) (fn_outlives f)));
+    try discriminate.
+  destruct (negb (wf_type_b (mk_region_ctx (fn_lifetimes f)) (fn_ret f)));
+    try discriminate.
+  destruct (negb (wf_params_b (mk_region_ctx (fn_lifetimes f)) (fn_params f)));
+    try discriminate.
+  destruct (infer_core_env env (fn_outlives f) (fn_lifetimes f)
+              (params_ctx (fn_params f)) (fn_body f))
+    as [[T_body Γ_out] | err] eqn:Hcore; try discriminate.
+  destruct (negb (wf_type_b (mk_region_ctx (fn_lifetimes f)) T_body));
+    try discriminate.
+  destruct (ty_compatible_b (fn_outlives f) T_body (fn_ret f))
+    eqn:Hcompatible; try discriminate.
+  destruct (params_ok_b (fn_params f) Γ_out) eqn:Hparams; try discriminate.
+  inversion H; subst.
+  exists T_body, Γ'.
+  repeat split.
+  - eapply infer_core_env_sound. exact Hcore.
+  - exact Hcompatible.
+  - exact Hparams.
 Qed.
 
 Lemma typed_fields_env_sound :
@@ -117,6 +169,20 @@ Lemma borrow_check_env_sound : forall env PBS Γ e PBS',
   borrow_ok_env env PBS Γ e PBS'.
 Proof.
   intros. constructor. exact H.
+Qed.
+
+Theorem infer_full_env_sound : forall env f T Γ',
+  infer_full_env env f = infer_ok (T, Γ') ->
+  checked_fn_env env f.
+Proof.
+  unfold infer_full_env, checked_fn_env.
+  intros env f T Γ' H.
+  destruct (infer_env env f) as [[T0 Γ0] | err] eqn:Hinfer; try discriminate.
+  destruct (borrow_check_env env [] (params_ctx (fn_params f)) (fn_body f))
+    as [PBS' | err] eqn:Hborrow; try discriminate.
+  split.
+  - eapply infer_env_sound. exact Hinfer.
+  - exists PBS'. eapply borrow_check_env_sound. exact Hborrow.
 Qed.
 
 (* ------------------------------------------------------------------ *)

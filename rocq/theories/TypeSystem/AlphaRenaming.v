3c120ded19e40cf4ab056b82ff32bf970f0fd2d4
Yuuki Takano <ytakanoster@gmail.com>
2026-05-15 19:53:48 +0000

    add forward alpha place helpers

--- rocq/theories/TypeSystem/AlphaRenaming.v
+++ rocq/theories/TypeSystem/AlphaRenaming.v
@@ -1,4 +1,4 @@
-From Facet.TypeSystem Require Import Types Syntax PathState Renaming TypingRules TypeChecker.
+From Facet.TypeSystem Require Import Types Syntax PathState Renaming TypingRules TypeChecker EnvStructuralRules.
 From Stdlib Require Import List String Bool Lia PeanoNat Program.Equality.
 Import ListNotations.
 
@@ -618,6 +618,171 @@ Proof.
     + exact H0.
   - simpl in Htyped_place. inversion Htyped_place; subst.
     eapply TP_Field; eauto.
+Qed.
+
+Lemma alpha_rename_typed_place_forward : forall fenv0 fenvr n ρ Γ0 Γr p T,
+  ctx_alpha ρ Γ0 Γr ->
+  ~ In (place_root p) (rename_range ρ) ->
+  typed_place fenv0 n Γ0 p T ->
+  typed_place fenvr n Γr (rename_place ρ p) T.
+Proof.
+  intros fenv0 fenvr n ρ Γ0 Γr p.
+  induction p as [x | p IH | p IH f]; intros T Hctx Hsafe Htyped_place.
+  - simpl. inversion Htyped_place; subst.
+    apply TP_Var.
+    eapply ctx_alpha_lookup_forward; eauto.
+  - simpl. inversion Htyped_place; subst.
+    eapply TP_Deref.
+    apply IH.
+    + exact Hctx.
+    + exact Hsafe.
+    + exact H0.
+  - simpl. inversion Htyped_place; subst.
+    eapply TP_Field; eauto.
+Qed.
+
+Lemma place_path_rename_place_some : forall ρ p x path,
+  place_path p = Some (x, path) ->
+  place_path (rename_place ρ p) = Some (lookup_rename x ρ, path).
+Proof.
+  intros ρ p.
+  induction p as [y | p IH | p IH f]; intros x path Hpath.
+  - simpl in Hpath. injection Hpath as <- <-. reflexivity.
+  - simpl in Hpath. discriminate.
+  - simpl.
+    simpl in Hpath.
+    remember (place_path p) as parent eqn:Hparent_eq.
+    symmetry in Hparent_eq.
+    destruct parent as [[y path0] |]; try discriminate.
+    specialize (IH y path0 eq_refl).
+    injection Hpath as <- <-.
+    rewrite IH.
+    reflexivity.
+Qed.
+
+Lemma place_path_rename_place_none : forall ρ p,
+  place_path p = None ->
+  place_path (rename_place ρ p) = None.
+Proof.
+  intros ρ p.
+  induction p as [x | p IH | p IH f]; intros Hpath.
+  - simpl in Hpath. discriminate.
+  - reflexivity.
+  - simpl.
+    simpl in Hpath.
+    remember (place_path p) as parent eqn:Hparent_eq.
+    symmetry in Hparent_eq.
+    destruct parent as [[x path] |]; try discriminate.
+    specialize (IH eq_refl).
+    rewrite IH.
+    reflexivity.
+Qed.
+
+Lemma place_path_root : forall p x path,
+  place_path p = Some (x, path) ->
+  place_root p = x.
+Proof.
+  fix IH 1.
+  intros p x path Hpath.
+  destruct p as [y | p | p f]; simpl in Hpath.
+  - injection Hpath as <- _. reflexivity.
+  - discriminate.
+  - destruct (place_path p) as [[y path0] |] eqn:Hparent;
+      try discriminate.
+    injection Hpath as <- _.
+    simpl. eapply IH. exact Hparent.
+Qed.
+
+Lemma alpha_rename_typed_place_type_env_structural_forward :
+  forall env ρ Σ Σr p T,
+    ctx_alpha ρ Σ Σr ->
+    ~ In (place_root p) (rename_range ρ) ->
+    typed_place_type_env_structural env Σ p T ->
+    typed_place_type_env_structural env Σr (rename_place ρ p) T.
+Proof.
+  intros env ρ Σ Σr p T Hctx Hsafe Hplace.
+  induction Hplace.
+  - simpl. eapply TPTES_Var.
+    eapply ctx_alpha_lookup_state_forward; eauto.
+  - simpl. eapply TPTES_Deref.
+    apply IHHplace. exact Hsafe.
+  - simpl. eapply TPTES_Field; eauto.
+Qed.
+
+Lemma alpha_rename_typed_place_env_structural_forward :
+  forall env ρ Σ Σr p T,
+    ctx_alpha ρ Σ Σr ->
+    ~ In (place_root p) (rename_range ρ) ->
+    typed_place_env_structural env Σ p T ->
+    typed_place_env_structural env Σr (rename_place ρ p) T.
+Proof.
+  intros env ρ Σ Σr p T Hctx Hsafe Hplace.
+  induction Hplace.
+  - simpl. eapply TPES_Var.
+    + eapply ctx_alpha_lookup_state_forward; eauto.
+    + exact H0.
+  - simpl. eapply TPES_Deref.
+    apply IHHplace. exact Hsafe.
+  - simpl. eapply TPES_Field.
+    + eapply alpha_rename_typed_place_type_env_structural_forward; eauto.
+    + exact H0.
+    + exact H1.
+    + exact H2.
+    + pose proof (place_path_rename_place_some ρ
+        (PField p (Program.field_name fdef)) x path H3) as Hpathr.
+      simpl in Hpathr. exact Hpathr.
+    + eapply ctx_alpha_lookup_state_forward.
+      * exact Hctx.
+      * rewrite <- (place_path_root
+          (PField p (Program.field_name fdef)) x path H3).
+        exact Hsafe.
+      * exact H4.
+    + exact H5.
+  - simpl. eapply TPES_Field_Indirect.
+    + eapply alpha_rename_typed_place_type_env_structural_forward; eauto.
+    + exact H0.
+    + exact H1.
+    + exact H2.
+    + pose proof (place_path_rename_place_none ρ
+        (PField p (Program.field_name fdef)) H3) as Hpathr.
+      simpl in Hpathr. exact Hpathr.
+Qed.
+
+Lemma alpha_rename_writable_place_env_structural_forward :
+  forall env ρ Σ Σr p,
+    ctx_alpha ρ Σ Σr ->
+    ~ In (place_root p) (rename_range ρ) ->
+    writable_place_env_structural env Σ p ->
+    writable_place_env_structural env Σr (rename_place ρ p).
+Proof.
+  intros env ρ Σ Σr p Hctx Hsafe Hwrite.
+  induction Hwrite.
+  - simpl. apply WPES_Var.
+    eapply ctx_alpha_lookup_mut_forward; eauto.
+  - simpl. eapply WPES_Deref.
+    eapply alpha_rename_typed_place_env_structural_forward; eauto.
+  - simpl. eapply WPES_Field.
+    + apply IHHwrite. exact Hsafe.
+    + eapply alpha_rename_typed_place_type_env_structural_forward; eauto.
+    + exact H0.
+    + exact H1.
+    + exact H2.
+    + exact H3.
+Qed.
+
+Lemma alpha_rename_place_under_unique_ref_structural_forward :
+  forall env ρ Σ Σr p,
+    ctx_alpha ρ Σ Σr ->
+    ~ In (place_root p) (rename_range ρ) ->
+    place_under_unique_ref_structural env Σ p ->
+    place_under_unique_ref_structural env Σr (rename_place ρ p).
+Proof.
+  intros env ρ Σ Σr p Hctx Hsafe Hunique.
+  induction Hunique.
+  - simpl. eapply PUURS_Deref.
+    eapply alpha_rename_typed_place_env_structural_forward; eauto.
+  - simpl. apply PUURS_Field.
+    apply IHHunique. exact Hsafe.
 Qed.
 
 Lemma expr_as_place_alpha_rename_some_backward : forall ρ used e er used' pr,

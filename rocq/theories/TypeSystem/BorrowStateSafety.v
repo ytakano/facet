72b518ef6a892715d5d3eeb72d640c907ad36283
Yuuki Takano <ytakanoster@gmail.com>
2026-05-15 12:12:14 +0000

    add borrow state pairwise conflicts

--- rocq/theories/TypeSystem/BorrowStateSafety.v
+++ rocq/theories/TypeSystem/BorrowStateSafety.v
@@ -17,6 +17,142 @@ Proof.
   exact I.
 Qed.
 
+Lemma ident_eqb_sym : forall x y,
+  ident_eqb x y = ident_eqb y x.
+Proof.
+  intros x y.
+  destruct (ident_eqb x y) eqn:Hxy.
+  - apply ident_eqb_eq in Hxy. subst y.
+    rewrite ident_eqb_refl. reflexivity.
+  - symmetry. apply ident_eqb_neq.
+    intro Heq. subst y.
+    rewrite ident_eqb_refl in Hxy. discriminate.
+Qed.
+
+Lemma path_conflict_b_sym : forall p q,
+  path_conflict_b p q = path_conflict_b q p.
+Proof.
+  intros p q.
+  unfold path_conflict_b.
+  rewrite Bool.orb_comm.
+  reflexivity.
+Qed.
+
+Lemma ident_path_conflict_b_sym : forall x p y q,
+  ident_eqb x y && path_conflict_b p q =
+  ident_eqb y x && path_conflict_b q p.
+Proof.
+  intros x p y q.
+  rewrite ident_eqb_sym.
+  rewrite path_conflict_b_sym.
+  reflexivity.
+Qed.
+
+Lemma pbs_no_conflicts_shared_mut_tail : forall x p y q rest,
+  pbs_no_conflicts (PBShared x p :: rest) ->
+  In (PBMut y q) rest ->
+  ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  intros x p y q rest Hnc Hin.
+  simpl in Hnc. destruct Hnc as [Hhas _].
+  eapply pbs_has_mut_false_no_conflict; eauto.
+Qed.
+
+Lemma pbs_no_conflicts_mut_shared_tail : forall x p y q rest,
+  pbs_no_conflicts (PBMut x p :: rest) ->
+  In (PBShared y q) rest ->
+  ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  intros x p y q rest Hnc Hin.
+  simpl in Hnc. destruct Hnc as [Hhas _].
+  eapply pbs_has_any_false_no_shared_conflict; eauto.
+Qed.
+
+Lemma pbs_no_conflicts_mut_mut_tail : forall x p y q rest,
+  pbs_no_conflicts (PBMut x p :: rest) ->
+  In (PBMut y q) rest ->
+  ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  intros x p y q rest Hnc Hin.
+  simpl in Hnc. destruct Hnc as [Hhas _].
+  eapply pbs_has_any_false_no_mut_conflict; eauto.
+Qed.
+
+Lemma pbs_no_conflicts_shared_mut_no_conflict :
+  forall PBS x p y q,
+    pbs_no_conflicts PBS ->
+    In (PBShared x p) PBS ->
+    In (PBMut y q) PBS ->
+    ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  induction PBS as [| h rest IH]; intros x p y q Hnc Hshared Hmut.
+  - contradiction.
+  - destruct h as [z r | z r]; simpl in Hnc.
+    + destruct Hnc as [Hhead Hrest].
+      simpl in Hshared, Hmut.
+      destruct Hshared as [Hshared | Hshared].
+      * injection Hshared as <- <-.
+        destruct Hmut as [Hmut | Hmut].
+        -- discriminate.
+        -- eapply pbs_has_mut_false_no_conflict; eauto.
+      * destruct Hmut as [Hmut | Hmut].
+        -- discriminate.
+        -- eapply IH; eauto.
+    + destruct Hnc as [Hhead Hrest].
+      simpl in Hshared, Hmut.
+      destruct Hshared as [Hshared | Hshared].
+      * discriminate.
+      * destruct Hmut as [Hmut | Hmut].
+        -- injection Hmut as <- <-.
+           rewrite ident_path_conflict_b_sym.
+           eapply pbs_has_any_false_no_shared_conflict; eauto.
+        -- eapply IH; eauto.
+Qed.
+
+Lemma pbs_no_conflicts_mut_shared_no_conflict :
+  forall PBS x p y q,
+    pbs_no_conflicts PBS ->
+    In (PBMut x p) PBS ->
+    In (PBShared y q) PBS ->
+    ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  intros PBS x p y q Hnc Hmut Hshared.
+  rewrite ident_path_conflict_b_sym.
+  eapply pbs_no_conflicts_shared_mut_no_conflict; eauto.
+Qed.
+
+Lemma pbs_no_conflicts_mut_mut_no_conflict :
+  forall PBS x p y q,
+    pbs_no_conflicts PBS ->
+    In (PBMut x p) PBS ->
+    In (PBMut y q) PBS ->
+    PBMut x p <> PBMut y q ->
+    ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  induction PBS as [| h rest IH]; intros x p y q Hnc Hmut1 Hmut2 Hneq.
+  - contradiction.
+  - destruct h as [z r | z r]; simpl in Hnc.
+    + destruct Hnc as [_ Hrest].
+      simpl in Hmut1, Hmut2.
+      destruct Hmut1 as [Hmut1 | Hmut1]; [discriminate |].
+      destruct Hmut2 as [Hmut2 | Hmut2]; [discriminate |].
+      eapply IH; eauto.
+    + destruct Hnc as [Hhead Hrest].
+      simpl in Hmut1, Hmut2.
+      destruct Hmut1 as [Hmut1 | Hmut1].
+      * destruct Hmut2 as [Hmut2 | Hmut2].
+        -- injection Hmut1 as <- <-.
+           injection Hmut2 as <- <-.
+           exfalso. apply Hneq. reflexivity.
+        -- injection Hmut1 as <- <-.
+           eapply pbs_has_any_false_no_mut_conflict; eauto.
+      * destruct Hmut2 as [Hmut2 | Hmut2].
+        -- injection Hmut2 as <- <-.
+           rewrite ident_path_conflict_b_sym.
+           eapply pbs_has_any_false_no_mut_conflict; eauto.
+        -- eapply IH; eauto.
+Qed.
+
 Lemma pbs_has_mut_remove_one_false : forall x p e PBS,
   pbs_has_mut x p PBS = false ->
   pbs_has_mut x p (pbs_remove_one e PBS) = false.
@@ -153,6 +289,53 @@ Proof.
   eapply Hpreserve.
   - eapply borrow_check_env_structural_sound. exact Hcheck.
   - exact Hnc.
+Qed.
+
+Corollary borrow_check_env_shared_mut_no_conflict :
+  forall env PBS Γ e PBS' x p y q,
+    borrow_check_env env PBS Γ e = infer_ok PBS' ->
+    pbs_no_conflicts PBS ->
+    In (PBShared x p) PBS' ->
+    In (PBMut y q) PBS' ->
+    ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  intros env PBS Γ e PBS' x p y q Hcheck Hnc Hshared Hmut.
+  eapply pbs_no_conflicts_shared_mut_no_conflict.
+  - eapply borrow_check_env_preserves_pbs_no_conflicts; eauto.
+  - exact Hshared.
+  - exact Hmut.
+Qed.
+
+Corollary borrow_check_env_mut_shared_no_conflict :
+  forall env PBS Γ e PBS' x p y q,
+    borrow_check_env env PBS Γ e = infer_ok PBS' ->
+    pbs_no_conflicts PBS ->
+    In (PBMut x p) PBS' ->
+    In (PBShared y q) PBS' ->
+    ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  intros env PBS Γ e PBS' x p y q Hcheck Hnc Hmut Hshared.
+  eapply pbs_no_conflicts_mut_shared_no_conflict.
+  - eapply borrow_check_env_preserves_pbs_no_conflicts; eauto.
+  - exact Hmut.
+  - exact Hshared.
+Qed.
+
+Corollary borrow_check_env_mut_mut_no_conflict :
+  forall env PBS Γ e PBS' x p y q,
+    borrow_check_env env PBS Γ e = infer_ok PBS' ->
+    pbs_no_conflicts PBS ->
+    In (PBMut x p) PBS' ->
+    In (PBMut y q) PBS' ->
+    PBMut x p <> PBMut y q ->
+    ident_eqb x y && path_conflict_b p q = false.
+Proof.
+  intros env PBS Γ e PBS' x p y q Hcheck Hnc Hmut1 Hmut2 Hneq.
+  eapply pbs_no_conflicts_mut_mut_no_conflict.
+  - eapply borrow_check_env_preserves_pbs_no_conflicts; eauto.
+  - exact Hmut1.
+  - exact Hmut2.
+  - exact Hneq.
 Qed.
 
 Corollary checked_fn_env_roots_borrow_state_no_conflicts :

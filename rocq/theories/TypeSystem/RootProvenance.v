8ce942026a83234116cdaa6e7be8ff2f6eab8e05
Yuuki Takano <ytakanoster@gmail.com>
2026-05-16 08:55:58 +0000

    add root substitution helpers

--- rocq/theories/TypeSystem/RootProvenance.v
+++ rocq/theories/TypeSystem/RootProvenance.v
@@ -61,6 +61,40 @@ Fixpoint root_env_remove (x : ident) (R : root_env) : root_env :=
       if ident_eqb x y then rest else (y, roots) :: root_env_remove x rest
   end.
 
+Definition root_subst := list (ident * root_set).
+
+Fixpoint root_subst_lookup (x : ident) (rho : root_subst) : root_set :=
+  match rho with
+  | [] => [x]
+  | (y, roots) :: rest =>
+      if ident_eqb x y then roots else root_subst_lookup x rest
+  end.
+
+Fixpoint root_set_instantiate (rho : root_subst) (roots : root_set)
+    : root_set :=
+  match roots with
+  | [] => []
+  | x :: rest =>
+      root_set_union (root_subst_lookup x rho)
+        (root_set_instantiate rho rest)
+  end.
+
+Fixpoint root_env_instantiate (rho : root_subst) (R : root_env)
+    : root_env :=
+  match R with
+  | [] => []
+  | (x, roots) :: rest =>
+      (x, root_set_instantiate rho roots) :: root_env_instantiate rho rest
+  end.
+
+Fixpoint root_subst_of_params (ps : list param) (arg_roots : list root_set)
+    : root_subst :=
+  match ps, arg_roots with
+  | p :: ps', roots :: arg_roots' =>
+      (param_name p, roots) :: root_subst_of_params ps' arg_roots'
+  | _, _ => []
+  end.
+
 Definition roots_exclude (x : ident) (roots : root_set) : Prop :=
   ~ In x roots.
 
@@ -211,6 +245,24 @@ Proof.
     exact IH.
 Qed.
 
+Lemma root_subst_lookup_eq :
+  forall x roots rho,
+    root_subst_lookup x ((x, roots) :: rho) = roots.
+Proof.
+  intros x roots rho. simpl.
+  rewrite ident_eqb_refl. reflexivity.
+Qed.
+
+Lemma root_subst_lookup_neq :
+  forall x y roots rho,
+    x <> y ->
+    root_subst_lookup x ((y, roots) :: rho) = root_subst_lookup x rho.
+Proof.
+  intros x y roots rho Hneq. simpl.
+  destruct (ident_eqb x y) eqn:Heq; try reflexivity.
+  apply ident_eqb_eq in Heq. contradiction.
+Qed.
+
 Lemma root_env_lookup_not_in_names :
   forall x R,
     ~ In x (root_env_names R) ->
@@ -257,6 +309,51 @@ Proof.
   unfold root_env_no_shadow.
   intros x roots R Hnodup.
   rewrite root_env_names_update. exact Hnodup.
+Qed.
+
+Lemma root_env_instantiate_names :
+  forall rho R,
+    root_env_names (root_env_instantiate rho R) = root_env_names R.
+Proof.
+  intros rho R.
+  induction R as [| [x roots] rest IH]; simpl; try reflexivity.
+  rewrite IH. reflexivity.
+Qed.
+
+Lemma root_env_instantiate_no_shadow :
+  forall rho R,
+    root_env_no_shadow R ->
+    root_env_no_shadow (root_env_instantiate rho R).
+Proof.
+  unfold root_env_no_shadow.
+  intros rho R Hnodup.
+  rewrite root_env_instantiate_names. exact Hnodup.
+Qed.
+
+Lemma root_env_lookup_instantiate :
+  forall x rho R roots,
+    root_env_lookup x R = Some roots ->
+    root_env_lookup x (root_env_instantiate rho R) =
+      Some (root_set_instantiate rho roots).
+Proof.
+  intros x rho R.
+  induction R as [| [y roots_y] rest IH]; intros roots Hlookup;
+    simpl in *; try discriminate.
+  destruct (ident_eqb x y) eqn:Heq.
+  - inversion Hlookup. reflexivity.
+  - eapply IH. exact Hlookup.
+Qed.
+
+Lemma root_env_lookup_instantiate_none :
+  forall x rho R,
+    root_env_lookup x R = None ->
+    root_env_lookup x (root_env_instantiate rho R) = None.
+Proof.
+  intros x rho R.
+  induction R as [| [y roots_y] rest IH]; intros Hlookup;
+    simpl in *; try reflexivity.
+  destruct (ident_eqb x y) eqn:Heq; try discriminate.
+  apply IH. exact Hlookup.
 Qed.
 
 Lemma root_env_no_shadow_add :
@@ -314,4 +411,28 @@ Proof.
   - simpl. rewrite Heq.
     apply IH.
     unfold root_env_no_shadow. exact Hnodup_tail.
+Qed.
+
+Lemma root_subst_of_params_lookup_head :
+  forall p ps roots arg_roots,
+    root_subst_lookup (param_name p)
+      (root_subst_of_params (p :: ps) (roots :: arg_roots)) = roots.
+Proof.
+  intros p ps roots arg_roots. simpl.
+  rewrite ident_eqb_refl. reflexivity.
+Qed.
+
+Lemma root_subst_of_params_lookup_not_in :
+  forall ps arg_roots x,
+    (forall p, In p ps -> param_name p <> x) ->
+    root_subst_lookup x (root_subst_of_params ps arg_roots) = [x].
+Proof.
+  intros ps.
+  induction ps as [| p ps IH]; intros arg_roots x Hnotin;
+    destruct arg_roots as [| roots arg_roots]; simpl; try reflexivity.
+  destruct (ident_eqb x (param_name p)) eqn:Heq.
+  - apply ident_eqb_eq in Heq. subst x.
+    exfalso. apply (Hnotin p); simpl; auto.
+  - apply IH. intros q Hinq.
+    apply Hnotin. simpl. right. exact Hinq.
 Qed.

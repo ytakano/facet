9d9c6517a2bcb235bef439929d99c1cb23040ef5
Yuuki Takano <ytakanoster@gmail.com>
2026-05-23 10:54:13 +0000

    extract call frame shadow safety

---
+++ rocq/theories/TypeSystem/TypeSafetyCallFrameShadowSafe.v
@@ -0,0 +1,218 @@
+From Facet.TypeSystem Require Import Lifetime Types Syntax PathState Program
+  Renaming OperationalSemantics TypingRules TypeChecker RuntimeTyping RootProvenance
+  EnvStructuralRules AlphaRenaming EnvSoundnessFacts CheckerSoundness.
+From Facet.TypeSystem Require Export TypeSafetyCallFrameRootEnv.
+From Stdlib Require Import List Bool ZArith String Program.Equality.
+Import ListNotations.
+
+Definition root_env_tail_fresh_names (R_tail : root_env)
+    (names : list ident) : Prop :=
+  forall x,
+    In x names ->
+    root_env_lookup x R_tail = None /\ root_env_excludes x R_tail.
+
+Lemma root_env_tail_fresh_names_app_l :
+  forall R_tail names1 names2,
+    root_env_tail_fresh_names R_tail (names1 ++ names2) ->
+    root_env_tail_fresh_names R_tail names1.
+Proof.
+  unfold root_env_tail_fresh_names.
+  intros R_tail names1 names2 Hfresh x Hin.
+  apply Hfresh. apply in_or_app. left. exact Hin.
+Qed.
+
+Lemma root_env_tail_fresh_names_app_r :
+  forall R_tail names1 names2,
+    root_env_tail_fresh_names R_tail (names1 ++ names2) ->
+    root_env_tail_fresh_names R_tail names2.
+Proof.
+  unfold root_env_tail_fresh_names.
+  intros R_tail names1 names2 Hfresh x Hin.
+  apply Hfresh. apply in_or_app. right. exact Hin.
+Qed.
+
+Lemma root_env_tail_fresh_names_cons_head :
+  forall R_tail x names,
+    root_env_tail_fresh_names R_tail (x :: names) ->
+    root_env_lookup x R_tail = None /\ root_env_excludes x R_tail.
+Proof.
+  unfold root_env_tail_fresh_names.
+  intros R_tail x names Hfresh.
+  apply Hfresh. left. reflexivity.
+Qed.
+
+Lemma root_env_tail_fresh_names_cons_tail :
+  forall R_tail x names,
+    root_env_tail_fresh_names R_tail (x :: names) ->
+    root_env_tail_fresh_names R_tail names.
+Proof.
+  unfold root_env_tail_fresh_names.
+  intros R_tail x names Hfresh y Hin.
+  apply Hfresh. right. exact Hin.
+Qed.
+
+Lemma root_env_tail_fresh_names_lookup_field :
+  forall R_tail fname fields e,
+    lookup_field_b fname fields = Some e ->
+    root_env_tail_fresh_names R_tail (fields_local_store_names fields) ->
+    root_env_tail_fresh_names R_tail (expr_local_store_names e).
+Proof.
+  unfold root_env_tail_fresh_names.
+  intros R_tail fname fields e Hlookup Hfresh x Hin.
+  apply Hfresh.
+  eapply fields_local_store_names_in_expr.
+  - eapply lookup_field_b_success. exact Hlookup.
+  - exact Hin.
+Qed.
+
+Theorem typed_roots_shadow_safe_tail_frame_mutual :
+  forall env Ω n R_tail,
+  (forall R Σ e T Σ' R' roots,
+    typed_env_roots_shadow_safe env Ω n R Σ e T Σ' R' roots ->
+    root_env_tail_fresh_names R_tail (expr_local_store_names e) ->
+    typed_env_roots_shadow_safe env Ω n (R ++ R_tail) Σ e T Σ'
+      (R' ++ R_tail) roots) /\
+  (forall R Σ args ps Σ' R' roots,
+    typed_args_roots_shadow_safe env Ω n R Σ args ps Σ' R' roots ->
+    root_env_tail_fresh_names R_tail (args_local_store_names args) ->
+    typed_args_roots_shadow_safe env Ω n (R ++ R_tail) Σ args ps Σ'
+      (R' ++ R_tail) roots) /\
+  (forall lts args R Σ fields defs Σ' R' roots,
+    typed_fields_roots_shadow_safe env Ω n lts args R Σ fields defs
+      Σ' R' roots ->
+    root_env_tail_fresh_names R_tail (fields_local_store_names fields) ->
+    typed_fields_roots_shadow_safe env Ω n lts args (R ++ R_tail) Σ
+      fields defs Σ' (R' ++ R_tail) roots).
+Proof.
+  intros env Ω n R_tail.
+  apply typed_roots_shadow_safe_ind; intros; simpl in *.
+  - constructor.
+  - constructor.
+  - constructor.
+  - constructor.
+  - eapply TERS_Var_Copy; eauto.
+    eapply root_env_lookup_app_left; eassumption.
+  - eapply TERS_Var_Move; eauto.
+    eapply root_env_lookup_app_left; eassumption.
+  - eapply TERS_Place_Copy; eauto.
+    eapply root_env_lookup_app_left; eassumption.
+  - eapply TERS_Place_Move; eauto.
+    eapply root_env_lookup_app_left; eassumption.
+  - eapply TERS_Call; eauto.
+  - eapply TERS_Fn; eauto.
+  - eapply TERS_MakeClosure; eauto.
+  - eapply TERS_MakeClosure_Static; eauto.
+  - eapply TERS_CallExpr_MakeClosure; eauto.
+  - eapply TERS_Struct; eauto.
+  - pose proof (root_env_tail_fresh_names_app_l _ _ _ H1) as Hfresh1.
+    pose proof (root_env_tail_fresh_names_app_r _ _ _ H1) as Hfresh_tail.
+    destruct (root_env_tail_fresh_names_cons_head _ _ _ Hfresh_tail)
+      as [Htail_lookup Htail_excl].
+    pose proof (root_env_tail_fresh_names_cons_tail _ _ _ Hfresh_tail)
+      as Hfresh2.
+    replace (root_env_remove x R2 ++ R_tail)
+      with (root_env_remove x (R2 ++ R_tail)).
+    eapply TERS_Let.
+    + apply H. exact Hfresh1.
+    + exact e.
+    + rewrite root_env_lookup_app_right by exact e0.
+      exact Htail_lookup.
+    + exact r.
+    + apply root_env_excludes_app; assumption.
+    + replace (root_env_add x roots1 R1 ++ R_tail)
+        with (root_env_add x roots1 (R1 ++ R_tail)) by reflexivity.
+      apply H0. exact Hfresh2.
+    + exact e3.
+    + exact r1.
+    + rewrite root_env_remove_app_left by exact Htail_lookup.
+      apply root_env_excludes_app; assumption.
+    + rewrite root_env_remove_app_left by exact Htail_lookup.
+      reflexivity.
+  - pose proof (root_env_tail_fresh_names_app_l _ _ _ H1) as Hfresh1.
+    pose proof (root_env_tail_fresh_names_app_r _ _ _ H1) as Hfresh_tail.
+    destruct (root_env_tail_fresh_names_cons_head _ _ _ Hfresh_tail)
+      as [Htail_lookup Htail_excl].
+    pose proof (root_env_tail_fresh_names_cons_tail _ _ _ Hfresh_tail)
+      as Hfresh2.
+    replace (root_env_remove x R2 ++ R_tail)
+      with (root_env_remove x (R2 ++ R_tail)).
+    eapply TERS_LetInfer.
+    + apply H. exact Hfresh1.
+    + rewrite root_env_lookup_app_right by exact e.
+      exact Htail_lookup.
+    + exact r.
+    + apply root_env_excludes_app; assumption.
+    + replace (root_env_add x roots1 R1 ++ R_tail)
+        with (root_env_add x roots1 (R1 ++ R_tail)) by reflexivity.
+      apply H0. exact Hfresh2.
+    + exact e0.
+    + exact r1.
+    + rewrite root_env_remove_app_left by exact Htail_lookup.
+      apply root_env_excludes_app; assumption.
+    + rewrite root_env_remove_app_left by exact Htail_lookup.
+      reflexivity.
+  - eapply TERS_Drop. eauto.
+  - replace (root_env_update x (root_set_union roots_old roots_new) R1 ++
+        R_tail)
+      with (root_env_update x (root_set_union roots_old roots_new)
+        (R1 ++ R_tail)).
+    eapply TERS_Replace_Path; eauto.
+    + eapply root_env_lookup_app_left; eassumption.
+    + eapply root_env_lookup_app_left; eassumption.
+    + rewrite root_env_update_app_left with (roots_old := roots_old)
+        by eassumption.
+      reflexivity.
+  - replace (root_env_update x (root_set_union roots_old roots_new) R1 ++
+        R_tail)
+      with (root_env_update x (root_set_union roots_old roots_new)
+        (R1 ++ R_tail)).
+    eapply TERS_Assign_Path; eauto.
+    + eapply root_env_lookup_app_left; eassumption.
+    + rewrite root_env_update_app_left with (roots_old := roots_old)
+        by eassumption.
+      reflexivity.
+  - eapply TERS_BorrowShared; eauto.
+  - eapply TERS_BorrowUnique; eauto.
+  - eapply TERS_DerefBorrowShared; eauto.
+    eapply root_env_lookup_app_left; eassumption.
+  - eapply TERS_DerefBorrowUnique; eauto.
+    eapply root_env_lookup_app_left; eassumption.
+  - pose proof (root_env_tail_fresh_names_app_l _ _ _ H2) as Hfresh1.
+    pose proof (root_env_tail_fresh_names_app_r _ _ _ H2) as Hfresh_tail.
+    pose proof (root_env_tail_fresh_names_app_l _ _ _ Hfresh_tail) as Hfresh2.
+    pose proof (root_env_tail_fresh_names_app_r _ _ _ Hfresh_tail) as Hfresh3.
+    eapply TERS_If.
+    + apply H. exact Hfresh1.
+    + exact e.
+    + apply H0. exact Hfresh2.
+    + apply H1. exact Hfresh3.
+    + exact e0.
+    + exact e4.
+    + apply root_env_equiv_app.
+      * exact r.
+      * apply root_env_equiv_refl.
+  - constructor.
+  - pose proof (root_env_tail_fresh_names_app_l _ _ _ H1) as Hfresh_e.
+    pose proof (root_env_tail_fresh_names_app_r _ _ _ H1) as Hfresh_es.
+    eapply TERSArgs_Cons.
+    + apply H. exact Hfresh_e.
+    + exact e0.
+    + apply H0. exact Hfresh_es.
+  - constructor.
+  - eapply TERSFields_Cons.
+    + exact e.
+    + apply H. eapply root_env_tail_fresh_names_lookup_field; eassumption.
+    + exact e0.
+    + apply H0. exact H1.
+Qed.
+
+Lemma typed_env_roots_shadow_safe_tail_frame :
+  forall env Ω n R_tail R Σ e T Σ' R' roots,
+    typed_env_roots_shadow_safe env Ω n R Σ e T Σ' R' roots ->
+    root_env_tail_fresh_names R_tail (expr_local_store_names e) ->
+    typed_env_roots_shadow_safe env Ω n (R ++ R_tail) Σ e T Σ'
+      (R' ++ R_tail) roots.
+Proof.
+  intros env Ω n R_tail.
+  exact (proj1 (typed_roots_shadow_safe_tail_frame_mutual env Ω n R_tail)).
+Qed.

91c5b3f5bce086a33cefa8b0ed060ee6e6fc71ea
Yuuki Takano <ytakanoster@gmail.com>
2026-05-27 19:05:40 +0000

    make AlphaRenaming compatibility aggregator

--- rocq/theories/TypeSystem/AlphaRenaming.v
+++ rocq/theories/TypeSystem/AlphaRenaming.v
@@ -1,5 +1,3 @@
-From Facet.TypeSystem Require Import Types Syntax PathState Program Renaming TypingRules RootProvenance TypeChecker EnvStructuralRules.
-From Facet.TypeSystem Require Export ExprFacts AlphaCore AlphaCtx AlphaPlace AlphaExpr AlphaFn AlphaTyping AlphaEnvStructural AlphaRoots AlphaShadowProvenance.
-From Stdlib Require Import List String Bool Lia PeanoNat Program.Equality.
-Import ListNotations.
-
+From Facet.TypeSystem Require Export
+  ExprFacts AlphaCore AlphaCtx AlphaPlace AlphaExpr AlphaFn
+  AlphaTyping AlphaEnvStructural AlphaRoots AlphaShadowProvenance.

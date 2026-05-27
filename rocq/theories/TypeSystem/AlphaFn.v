658f307552a0fa2230ddf28c1d3ca35f25f8f11c
Yuuki Takano <ytakanoster@gmail.com>
2026-05-27 16:41:11 +0000

    split function alpha relations

---
+++ rocq/theories/TypeSystem/AlphaFn.v
@@ -0,0 +1,39 @@
+From Facet.TypeSystem Require Import Syntax.
+From Facet.TypeSystem Require Import AlphaExpr.
+From Stdlib Require Import List.
+Import ListNotations.
+
+(* ------------------------------------------------------------------ *)
+(* Function environment alpha-renaming relations                       *)
+(* ------------------------------------------------------------------ *)
+
+Definition same_fn_shape (f fr : fn_def) : Prop :=
+  fn_name f = fn_name fr /\
+  fn_ret f = fn_ret fr /\
+  params_alpha (fn_params f) (fn_params fr).
+
+Inductive fenv_alpha : list fn_def -> list fn_def -> Prop :=
+  | FenvAlpha_Nil :
+      fenv_alpha [] []
+  | FenvAlpha_Cons : forall f fr fs fsr,
+      same_fn_shape f fr ->
+      fenv_alpha fs fsr ->
+      fenv_alpha (f :: fs) (fr :: fsr).
+
+Lemma fenv_alpha_in_backward : forall fenv fenvr fr,
+  fenv_alpha fenv fenvr ->
+  In fr fenvr ->
+  exists f,
+    In f fenv /\
+    same_fn_shape f fr.
+Proof.
+  intros fenv fenvr fr Halpha Hin.
+  induction Halpha.
+  - contradiction.
+  - simpl in Hin.
+    destruct Hin as [Heq | Hin].
+    + subst fr. exists f. split; [left; reflexivity | assumption].
+    + destruct (IHHalpha Hin) as [f0 [Hin0 Hshape]].
+      exists f0. split; [right; exact Hin0 | exact Hshape].
+Qed.
+
--- rocq/theories/TypeSystem/AlphaRenaming.v
+++ rocq/theories/TypeSystem/AlphaRenaming.v
@@ -1,5 +1,5 @@
 From Facet.TypeSystem Require Import Types Syntax PathState Program Renaming TypingRules RootProvenance TypeChecker EnvStructuralRules.
-From Facet.TypeSystem Require Export ExprFacts AlphaCore AlphaCtx AlphaPlace AlphaExpr.
+From Facet.TypeSystem Require Export ExprFacts AlphaCore AlphaCtx AlphaPlace AlphaExpr AlphaFn.
 From Stdlib Require Import List String Bool Lia PeanoNat Program.Equality.
 Import ListNotations.
 
@@ -52,36 +52,6 @@ Proof.
         -- unfold same_param_shape. simpl. split; reflexivity.
         -- exact Halpha_tail.
       * rewrite Hrename_tail. reflexivity.
-Qed.
-
-Definition same_fn_shape (f fr : fn_def) : Prop :=
-  fn_name f = fn_name fr /\
-  fn_ret f = fn_ret fr /\
-  params_alpha (fn_params f) (fn_params fr).
-
-Inductive fenv_alpha : list fn_def -> list fn_def -> Prop :=
-  | FenvAlpha_Nil :
-      fenv_alpha [] []
-  | FenvAlpha_Cons : forall f fr fs fsr,
-      same_fn_shape f fr ->
-      fenv_alpha fs fsr ->
-      fenv_alpha (f :: fs) (fr :: fsr).
-
-Lemma fenv_alpha_in_backward : forall fenv fenvr fr,
-  fenv_alpha fenv fenvr ->
-  In fr fenvr ->
-  exists f,
-    In f fenv /\
-    same_fn_shape f fr.
-Proof.
-  intros fenv fenvr fr Halpha Hin.
-  induction Halpha.
-  - contradiction.
-  - simpl in Hin.
-    destruct Hin as [Heq | Hin].
-    + subst fr. exists f. split; [left; reflexivity | assumption].
-    + destruct (IHHalpha Hin) as [f0 [Hin0 Hshape]].
-      exists f0. split; [right; exact Hin0 | exact Hshape].
 Qed.
 
 Lemma lookup_rename_in_range_or_self : forall ρ x,

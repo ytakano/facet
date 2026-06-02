d0cb7865bbc3d119a7801f03f234de26a956bda3
Yuuki Takano <ytakanoster@gmail.com>
2026-06-02 22:16:42 +0000

    add zero fuel generic value bridge

--- rocq/theories/TypeSystem/EnvRuntimeBaseSafety.v
+++ rocq/theories/TypeSystem/EnvRuntimeBaseSafety.v
@@ -11248,6 +11248,7 @@ Record generic_direct_call_runtime_package
     store_function_closure_targets_summary env s'
 }.
 
+
 Lemma eval_generic_direct_call_store_safe_narrow_summary_value_prefix_named_expr :
   forall env Omega n R Sigma fname type_args args sigma Sigma_args R_args
       arg_roots s s' ret fdef,
@@ -11466,6 +11467,42 @@ Proof.
     eassumption.
 Qed.
 
+Lemma eval_generic_direct_call_store_safe_narrow_summary_value_prefix_named_fuel_zero :
+  forall env Omega n R Sigma fname type_args args sigma Sigma_args R_args
+      arg_roots s s' ret fdef,
+    store_safe_function_value_call_args env args ->
+    callee_body_root_shadow_store_safe_narrow_summary_instantiated_fuel
+      env 0 fdef type_args ->
+    store_typed_prefix env s Sigma ->
+    store_roots_within R s ->
+    store_no_shadow s ->
+    root_env_no_shadow R ->
+    root_env_store_roots_named R s ->
+    root_env_store_keys_named R s ->
+    store_function_closure_targets_summary env s ->
+    eval env s (ECallGeneric fname type_args args) s' ret ->
+    fn_env_unique_by_name env ->
+    In fdef (env_fns env) ->
+    fn_name fdef = fname ->
+    fn_captures fdef = [] ->
+    typed_args_roots env Omega n R Sigma args
+      (apply_lt_params sigma
+        (apply_type_params type_args (fn_params fdef)))
+      Sigma_args R_args arg_roots ->
+    Forall (fun '(a, b) => outlives Omega a b)
+      (apply_lt_outlives sigma (fn_outlives fdef)) ->
+    value_has_type env s' ret
+      (apply_lt_ty sigma (subst_type_params_ty type_args (fn_ret fdef))).
+Proof.
+  intros env Omega n R Sigma fname type_args args sigma Sigma_args R_args
+    arg_roots s s' ret fdef Hsafe_args Hsummary Hstore Hroots Hshadow Hrn
+    Hnamed Hkeys Hsummary_store Heval Hunique Hin Hname Hcaps Htyped_args
+    Houtlives.
+  eapply eval_generic_direct_call_store_safe_narrow_summary_value_prefix_named;
+    try eassumption.
+  eapply callee_body_root_shadow_store_safe_narrow_summary_instantiated_of_fuel_zero.
+  exact Hsummary.
+Qed.
 
 Definition callee_body_root_shadow_captured_call_generic_direct_narrow_store_safe_summary
     (env : global_env) (fdef : fn_def) : Prop :=
